c7c8ba7502034f23302bfa699e9972f8dca360b4
Karoll Guzmán <karolldguzman@gmail.com>
2025-03-28 00:36:16 +0000

    endpoints

--- src/api/routes.py
+++ src/api/routes.py
@@ -20,3 +20,36 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/signup', methods=['POST'])
+def create_user():
+    body = request.get_json()
+    user_email = body['email']
+    user_password = hashlib.sha256(body['password'].encode("utf-8")).hexdigest()
+    user = User(email = user_email, password = user_password)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify("New user created")
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    body = request.get_json()
+    user_email = body['email']
+    user_password = hashlib.sha256(body['password']).encode("utf-8")
+    user = User.query.filter_by(email = user_email, password = user_password).first()
+    if user and user.password == user_password:
+        access_token = create_access_token(identity = user.id)
+        return jsonify(access_token = access_token, user = user)
+    else:
+        return jsonify("User does not exist")
+
+
+@api.route('/user', methods=['GET'])
+@jwt_required
+def get_user():
+    uid = get_jwt_identity()
+    user = User.query.filter_by(id=uid).first()
+    return jsonify(user)
